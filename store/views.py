b2f1bf23e314a33125c8598cc29b7447bdafc943
21Sirius <21siriusss@gmail.com>
2023-11-16 23:26:32 +0700

    Добавление элементов в базу данных

--- store/views.py
+++ store/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Product
+
+
+def build_template(lst: list, cols: int) -> list[list]:
+    return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
-    return HttpResponse("<h1>Hello</h1")
+    products = Product.objects.all()
+    return render(
+        request,
+        "store/product_list.html",
+        context={"product_list": build_template(products, 3)}
+
+    )
