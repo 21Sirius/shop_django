4212f26ff00fdb991312e7fa290b9e8782e014fe
21Sirius <21siriusss@gmail.com>
2023-11-17 20:47:19 +0700

    Работа с детальным просмотром каждого товара

--- store/models.py
+++ store/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Product(models.Model):
-
     title = models.CharField(max_length=150, db_index=True)
     info = models.TextField(blank=True)
     price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return self.title
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import *
 
 
 def build_template(lst: list, cols: int) -> list[list]:
@@ -8,10 +8,24 @@ def build_template(lst: list, cols: int) -> list[list]:
 
 
 def product_list(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
     return render(
         request,
         "store/product_list.html",
-        context={"product_list": build_template(products, 3)}
+        context={"product_list": build_template(products, 3),
+                 "categories": categories
+                 }
+    )
 
+
+def product_detail(request, pk):
+    categories = Category.objects.all()
+    product = Product.objects.get(pk=pk)
+    return render(
+        request,
+        "store/product_detail.html",
+        context={"product": product,
+                 "catrgories": categories
+                 }
     )
